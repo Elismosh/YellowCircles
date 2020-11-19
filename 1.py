ce6e74e2143bfcc55f67c21439865d4792708d44
Elismosh <74546372+Elismosh@users.noreply.github.com>
2020-11-19 22:16:34 +0300

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,37 @@
+import sys
+from random import randrange
+
+from PyQt5.QtGui import QPainter, QColor
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from Yellow import Ui_MainWindow
+
+
+class MyWidget(QMainWindow, Ui_MainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+        self.pushButton.clicked.connect(self.paint)
+        self.pushButton.setStyleSheet('QPushButton {background-color: #e32636; color: white; font-size: 17px;}')
+
+    def run(self, qp):
+        qp.setBrush(QColor('#ffcc00'))
+        d = randrange(20, 400)
+        x = randrange(0, 800 - d)
+        y = randrange(0, 600 - d)
+        qp.drawEllipse(x, y, d, d)
+
+    def paint(self):
+        self.repaint()
+
+    def paintEvent(self, event):
+        qp = QPainter()
+        qp.begin(self)
+        self.run(qp)
+        qp.end()
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = MyWidget()
+    ex.show()
+    sys.exit(app.exec_())
